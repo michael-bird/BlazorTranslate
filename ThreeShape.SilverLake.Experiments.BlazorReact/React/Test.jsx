9d296f059388633be04f60cbbf3a66c0028a7f04
michael-bird <94964585+michael-bird@users.noreply.github.com>
2022-01-06 12:55:21 +0100

    Update Test.jsx

--- ThreeShape.SilverLake.Experiments.BlazorReact/React/Test.jsx
+++ ThreeShape.SilverLake.Experiments.BlazorReact/React/Test.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom'
 import ComboBox from './ComboBox'
 
 export default function renderHello(element, serialisedItems) {
-    console.log(serialisedItems);
+
     const items = JSON.parse(serialisedItems);
-    console.log(items);
+
     ReactDOM.render(<ComboBox data={items} textField="Name" />, element);
 }
 
