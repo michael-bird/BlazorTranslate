be9347078c1c676f0b88efe7d07c018ca0c2a4be
Oksana Babaienko <Oksana.Babaienko@3shape.com>
2022-02-03 11:27:11 +0200

    SIL165 Solution added and readme file updated

---
+++ ThreeShape.SilverLake.Experiments.SIL165/ThreeShape.SilverLake.Experiments.SIL165/Views/BaseView.cs
@@ -0,0 +1,9 @@
+﻿using ThreeShape.SilverLake.Experiments.SIL165.Drivers;
+
+namespace ThreeShape.SilverLake.Experiments.SIL165.Views
+{
+    public class BaseView
+    {
+        public ChromeBrowser Browser { get; set; }
+    }
+}
--- ThreeShape.SilverLake.Experiments.TestAutomation/ThreeShape.SilverLake.Experiments.TestAutomation/Views/BaseView.cs
+++
@@ -1,9 +0,0 @@
-﻿using ThreeShape.SilverLake.Experiments.TestAutomation.Drivers;
-
-namespace ThreeShape.SilverLake.Experiments.TestAutomation.Views
-{
-    public class BaseView
-    {
-        public ChromeBrowser Browser { get; set; }
-    }
-}
